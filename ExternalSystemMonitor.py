73220e50ec4eda0aac8eeac41123c029a28f7aab
Marco Platino <mistermarcop@hotmail.com>
2025-06-30 16:26:50 -0400

    Updated the display so that it shows the CPU temperature, CPU load, RAM usage, and disk space. The display updates every second without clearing the previous messages.

--- ExternalSystemMonitor.py
+++ ExternalSystemMonitor.py
@@ -2,3 +2,13 @@ from LCD import LCD
 import time
 import system
 
+lcd = LCD(2, 0x27, True) #You have to say 2, because this is a different library but it works for 4 row LCDS.
+
+lcd.clear()
+
+while True:
+    lcd.message(f'CPU temp: {str(system.temperature())} C', 1)
+    lcd.message(f'CPU load: {system.cpuLoad()}', 2)
+    lcd.message(f'RAM usage: {str(system.RAM())}%', 3)
+    lcd.message(f'Disk: {system.diskspace(1)}', 4)
+    time.sleep(1)
--- system.py
+++ system.py
@@ -1,4 +1,24 @@
+import psutil
+
 def temperature():
     with open('/sys/class/thermal/thermal_zone0/temp', 'r') as file:
             cpu_temp = float(file.read()) / 1000
-    print(f"CPU Temperature: {cpu_temp}°C")
+    # return (f"CPU Temperature: {cpu_temp}°C")
+    return cpu_temp
+
+def diskspace(r):
+    def GB(amount):
+        return amount/1073741824
+    disk = psutil.disk_usage('/')
+    if r == 1:
+        return f'{round(GB(disk.used), 1)}/{round(GB(disk.total), 1)} GB'
+    else: 
+         return f'({disk.percent}%)'
+def cpuLoad():
+     return f'{psutil.cpu_percent(interval=1)}%'
+
+def RAM():
+    ram = psutil.virtual_memory()
+    return ram.percent
+if __name__ == "__main__": #Just some stuff for some testing and whatever...
+    print(cpuLoad())  
